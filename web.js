16a04e658d4ac2f780e661288ded778e4999c4c9
Jason de Oliveira <jdeoliveira@northpointdigital.com>
2015-04-28 14:14:33 -0400

    Update web.js

--- web.js
+++ web.js
@@ -12,7 +12,23 @@ addon.webhook('room_message', /@[Dd]orman/, function *() {
     "I left the company to pursue my dreams. If you need to reach me, I'll be sitting on my couch drinking whiskey and yelling at my TV.",
     "Let me google that for you.",
     "WordWhat?",
-    "I love Druuuu-pall!"
+    "I love Druuuu-pall!",
+    "No. Get back to work.",
+    "Should I take the rest of the month off?",
+    "Does Drupal API have more information on that?",
+    "Ummmm, yeah... I'm going to need you to go ahead and come in on Saturday.",
+    "Nevermind that. Did you update your timesheet?",
+    "That is a veiled HR VIOLATION.",
+    "Put $2.00 in the jar.",
+    "I want my $2.00.",
+    "Collect your pink slip at Nicole's desk.",
+    "Prontos?",
+    "Does Evan still work here?",
+    "Is it 4:20 yet?",
+    "It's Jason's fault.",
+    "I'm wearing jeans right now...",
+    "/me is replying...",
+    "/me left the room (user disconnected)"
   ];
 
   yield this.roomClient.sendNotification(phrases[Math.floor(Math.random() * phrases.length)]);
